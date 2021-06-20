c6f3455642b95f109b3f4e163b78924a3fdcc0a0
kou0402 <kou.4869.biz@gmail.com>
2021-06-20 23:34:12 +0900

    refactor: archi api

--- src/domain/archis/dto/create-archi.dto.ts
+++ src/domain/archis/dto/create-archi.dto.ts
@@ -1,7 +1,7 @@
 export class CreateArchiRequestDto {
   title: string
-  type: 'Webアプリ' | 'スマホアプリ'
-  scale: '個人開発' | '小規模' | '中規模' | '大規模'
+  type: string
+  scale: string
   author: string
   description?: string
   frontElements?: string[]
